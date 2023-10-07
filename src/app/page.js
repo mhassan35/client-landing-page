7eb267476cea3dd367c7a26ac9c5c972065d379a
M-Hassan1 <hassan81932@gmail.com>
2023-10-07 07:56:28 +0500

    Pushed Again

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import ThirdPage from "@/components/View/Footer/FooterData/ThirdPage";
 import Footer from "@/components/View/Footer/FooterFile/Footer";
 import Navbar from "@/components/View/Header/NavBar/navBar";
 import Category from "@/components/View/HeroSection/Category/Category";
 import HomePage from "@/components/View/HeroSection/Home/homePage";
+import ProductCarousel from "@/components/View/HeroSection/Reviews/Test";
 import Internships from "@/components/View/HeroSection/internships/Internships";
 
 
@@ -14,6 +14,7 @@ export default function Home() {
     <Category />
     <Internships />
     <Footer />    
+    <ProductCarousel />
   </div>
   )
 }
--- src/components/View/HeroSection/internships/Internships.js
+++ src/components/View/HeroSection/internships/Internships.js
@@ -1,112 +1,5 @@
-
-
 'use client'
-
-//  const LatestInternships = () => {
-//   return (
-//     <di>
-//     <div className='items-center pt-16 justify-center text-center'>
-//       <h1 className="font-semibold text-4xl leading-11 mb-6">
-//         Latest Internships
-//       </h1>
-//       <ul className="flex flex-col items-center sm:flex-row cursor-pointer justify-center">
-//           <li className='px-4 py-4 rounded-md text-lg'>
-//             First
-//           </li>
-          
-//           <li className='px-4 py-4 rounded-md text-lg'>
-//             First
-//           </li>
-          
-//           <li className='px-4 py-4 rounded-md text-lg'>
-//             First
-//           </li>
-          
-//           <li className='px-4 py-4 rounded-md text-lg'>
-//             First
-//           </li>
-          
-//           <li className='px-4 py-4 rounded-md text-lg'>
-//             First
-//           </li>
-//       </ul>
-//     </div>
-//     <div>
-//       <h1>
-//           Other slick data
-//       </h1>
-//     </div>
-//     </di>
-//   );
-// };
-
-// export default LatestInternships;
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// const LatestInternships = () => {
-//   return (
-//     <div className='pt-16 text-center'>
-//       <h1 className="font-semibold text-4xl leading-11 mb-6">
-//         Latest Internships
-//       </h1>
-//       <ul className="flex flex-col items-center sm:flex-row justify-center">
-//         <li className='px-4 py-4 rounded-md text-lg mb-2 sm:mb-0'>
-//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//             First
-//           </button>
-//         </li>
-//         <li className='px-4 py-4 rounded-md text-lg mb-2 sm:mb-0'>
-//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//             Second
-//           </button>
-//         </li>
-//         <li className='px-4 py-4 rounded-md text-lg mb-2 sm:mb-0'>
-//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//             Third
-//           </button>
-//         </li>
-//         <li className='px-4 py-4 rounded-md text-lg mb-2 sm:mb-0'>
-//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//             Fourth
-//           </button>
-//         </li>
-//         <li className='px-4 py-4 rounded-md text-lg'>
-//           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-//             Fifth
-//           </button>
-//         </li>
-//       </ul>
-//       <div>
-//         <h1>Other slick data</h1>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LatestInternships;
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
-import { BsArrowRightShort } from 'react-icons/bs';
 
 import { MdOutlineAccountBox } from 'react-icons/md';
 import { BiSolidShoppingBag ,BiSolidColor } from 'react-icons/bi';
@@ -116,6 +9,8 @@ import { MdEngineering } from 'react-icons/md';
 import { AiOutlineTranslation } from 'react-icons/ai';
 
 import Link from 'next/link';
+import FirstPage from './FirstPage';
+// import CarouselSection from './carouselSection';
 
 
 
@@ -150,7 +45,7 @@ const LatestInternships = () => {
               selectedButton === index ? 'bg-blue-700 text-white' : ' hover:bg-blue-700 text-black'
             }`}
           >
-            <Link href=""
+            <Link href="#"
               onClick={() => handleButtonClick(index)}
               className="w-full h-full flex items-center justify-center"
             >
@@ -161,7 +56,7 @@ const LatestInternships = () => {
         ))}
       </ul>
       <div>
-        <h1>Other slick data</h1>
+      <FirstPage />
       </div>
     </div>
   );
